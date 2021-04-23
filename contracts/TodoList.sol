5ad3cf8ee8ff96e80d54df7380606cee4e83a5bc
Andrei Iliescu <andrei.iliescu@stonesouptech.com>
2021-04-24 00:31:39 +0300

    update app and TodoList smart contract + migration

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,23 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.5.0;
 
 contract TodoList {
-    uint public taskCount = 0;
+	uint public taskCount = 0; //state variable
+
+  struct Task {
+		uint id;
+		string content;
+		bool completed;
+  }
+
+	mapping(uint => Task) public tasks; //we made a key value pair
+
+	constructor() public {
+		createTask("Primul task");
+	}
+
+	function createTask(string memory _content) public {
+		taskCount ++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+	}
+
 }
